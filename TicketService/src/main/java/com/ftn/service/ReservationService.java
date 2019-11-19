85dac41e59f6e952726b4b1f3e179df91d759629
Nemanja Milosavljevic <nemanjam2008@gmail.com>
2019-11-19 18:00:01 +0100

    Basic services for reservation, role, ticket and user.

--- TicketService/src/main/java/com/ftn/service/ReservationService.java
+++ TicketService/src/main/java/com/ftn/service/ReservationService.java
@@ -1,4 +1,40 @@
 package com.ftn.service;
 
+
+import com.ftn.model.Reservation;
+import com.ftn.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReservationService {
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    public List<Reservation> finfAllReservation(){
+        return reservationRepository.findAll();
+    }
+
+    public Reservation findOneReservation(Long id){
+        return reservationRepository.findById(id).orElse(null);
+    }
+
+    public void addReservation(Reservation r){
+        reservationRepository.save(r);
+    }
+
+    public void deleteReservation(Long id){
+        reservationRepository.deleteById(id);
+    }
+
+    public void deleteAll(){
+        reservationRepository.deleteAll();
+    }
+
+    public Boolean ifExist(Long id){
+        return reservationRepository.existsById(id);
+    }
 }
--- TicketService/src/main/java/com/ftn/service/RoleService.java
+++ TicketService/src/main/java/com/ftn/service/RoleService.java
@@ -1,4 +1,39 @@
 package com.ftn.service;
 
+import com.ftn.model.Role;
+import com.ftn.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RoleService {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    public List<Role> finfAllRoles(){
+        return roleRepository.findAll();
+    }
+
+    public Role findOneRole(Long id){
+        return roleRepository.findById(id).orElse(null);
+    }
+
+    public void addRole(Role u){
+        roleRepository.save(u);
+    }
+
+    public void deleteRole(Long id){
+        roleRepository.deleteById(id);
+    }
+
+    public void deleteAll(){
+        roleRepository.deleteAll();
+    }
+
+    public Boolean ifExist(Long id){
+        return roleRepository.existsById(id);
+    }
 }
